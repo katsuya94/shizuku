dedda6f243e8e58de47bf9cf2127c18159d8459c
atateno <adrien.tateno@getbraintree.com>
2018-06-22 11:15:56 -0500

    Set user password as root

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,6 @@ set -o nounset
 set -o pipefail
 
 useradd -G sudo,docker -m -s /bin/bash kacchan
+passwd kacchan
 mkdir -p /home/kacchan/.ssh
 cp /root/.ssh/authorized_keys /home/kacchan/.ssh/authorized_keys
-su -l kacchan -c passwd
