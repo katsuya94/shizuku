84f3f9b7a9a6fb5056498f12c32242eccedba9ea
atateno <adrien.tateno@getbraintree.com>
2018-06-22 16:35:10 -0500

    Update zkCli.sh to use shizuku network

--- bin/zkCli.sh
+++ bin/zkCli.sh
@@ -4,5 +4,6 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-exec docker run -it --rm --link shizuku_zookeeper_1:zookeeper zookeeper:3.4 \
+exec docker run -it --rm --network shizuku_default \
+  --link shizuku_zookeeper_1:zookeeper zookeeper:3.4 \
   zkCli.sh -server zookeeper "$@"
