8d1db33a6f95c288b867ba80a621b9055f8438a7
atateno <adrien.tateno@getbraintree.com>
2018-06-22 16:32:29 -0500

    Fix zookeepr container name

--- bin/zkCli.sh
+++ bin/zkCli.sh
@@ -4,5 +4,5 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-exec docker run -it --rm --link shizuku_zookeeper:zookeeper zookeeper:3.4 \
+exec docker run -it --rm --link shizuku_zookeeper_1:zookeeper zookeeper:3.4 \
   zkCli.sh -server zookeeper "$@"
