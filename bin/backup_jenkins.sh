12e4b8b488ac929e14f6861d0df540676639940a
atateno <adrien.tateno@getbraintree.com>
2018-06-22 16:13:00 -0500

    Fix archive paths

--- bin/backup_jenkins.sh
+++ bin/backup_jenkins.sh
@@ -5,10 +5,10 @@ set -o nounset
 set -o pipefail
 
 PROJECT_ROOT="$(dirname $(dirname ${BASH_SOURCE[0]}))"
-ARCHIVE_PATH="/root/jenkins_home-$(date "+%Y%m%d%H%M%S").tar.gz"
+ARCHIVE_FILENAME="jenkins_home-$(date "+%Y%m%d%H%M%S").tar.gz"
 
 docker-compose -f $PROJECT_ROOT/docker-compose.yaml stop jenkins
 docker run --rm --volumes-from shizuku_jenkins_1 -v ~:/root ubuntu \
-  tar cvzf $ARCHIVE_PATH -C /var jenkins_home
-sudo chown $(id -u):$(id -g) $ARCHIVE_PATH
+  tar cvzf /root/$ARCHIVE_FILENAME -C /var jenkins_home
+sudo chown $(id -u):$(id -g) ~/$ARCHIVE_FILENAME
 docker-compose -f $PROJECT_ROOT/docker-compose.yaml start jenkins
