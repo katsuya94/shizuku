575cb6309f80e79b76a58b1ac728998f8b5d2c1c
atateno <adrien.tateno@getbraintree.com>
2018-06-22 14:36:19 -0500

    Add certificate subject

--- bootstrap.sh
+++ bootstrap.sh
@@ -37,7 +37,8 @@ else
 
   # self-sign server certificate
   openssl req -x509 -key $PROJECT_ROOT/nginx/ssl/server.key \
-    -out $PROJECT_ROOT/nginx/ssl/server.crt
+    -out $PROJECT_ROOT/nginx/ssl/server.crt -days 365 \
+    -subj "/C=US/ST=Illinois/L=Chicago/O=atateno.io/OU=atateno.io/CN=atateno.io"
 
   # restrict server key permissions to the nginx user
   chmod 400 $PROJECT_ROOT/nginx/ssl/server.key
