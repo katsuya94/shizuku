cd335b7d8f06562edd1ac5bfa2550cfcf09944b9
atateno <adrien.tateno@getbraintree.com>
2018-06-22 16:10:18 -0500

    Fix path and permissions in backup script

--- bin/backup_jenkins.sh
+++ bin/backup_jenkins.sh
@@ -5,8 +5,10 @@ set -o nounset
 set -o pipefail
 
 PROJECT_ROOT="$(dirname $(dirname ${BASH_SOURCE[0]}))"
+ARCHIVE_PATH="/root/jenkins_home-$(date "+%Y%m%d%H%M%S").tar.gz"
 
 docker-compose -f $PROJECT_ROOT/docker-compose.yaml stop jenkins
 docker run --rm --volumes-from shizuku_jenkins_1 -v ~:/root ubuntu \
-  tar cvzf /root/jenkins-$(date "+%Y%m%d%H%M%S").tar.gz /var/jenkins_home
+  tar cvzf $ARCHIVE_PATH -C /var jenkins_home
+sudo chown $(id -u):$(id -g) $ARCHIVE_PATH
 docker-compose -f $PROJECT_ROOT/docker-compose.yaml start jenkins
