2eb1de1e362b2febc18194c0c00c55b2dcc5ce84
atateno <adrien.tateno@getbraintree.com>
2018-06-22 10:46:22 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,6 @@
-adduser -G sudo kacchan
+#!/usr/bin/env bash
+# DigitalOcean Docker 17.12.0~ce on 16.04
+adduser -G sudo, docker kacchan
 mkdir -p /home/kacchan/.ssh
 cp /root/.ssh/authorized_keys /home/kacchan/.ssh/authorized_keys
 su -l kacchan passwd
