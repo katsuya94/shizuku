69a66909fe39c0e125902db90f46169c1a7eebd5
atateno <adrien.tateno@getbraintree.com>
2018-06-22 10:51:21 -0500

    Update bootstrap.sh

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# shizuku
+# Shizuku
+
+## Bootstrapping
+
+```sh
+ssh -i ~/.ssh/id_rsa root@ip.address
+git clone https://github.com/katsuya94/shizuku.git
+shizuku/bootstrap.sh
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 # DigitalOcean Docker 17.12.0~ce on 16.04
-adduser -G sudo, docker kacchan
+set -o errexit
+set -o verbose
+set -o nounset
+set -o pipefail
+
+useradd -G sudo, docker kacchan
 mkdir -p /home/kacchan/.ssh
 cp /root/.ssh/authorized_keys /home/kacchan/.ssh/authorized_keys
 su -l kacchan passwd
