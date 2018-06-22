8294f7b5466f7ab66a533508632482706bf2534f
atateno <adrien.tateno@getbraintree.com>
2018-06-22 11:33:26 -0500

    Add sshd_config patch

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,11 +2,13 @@
 # DigitalOcean Docker 17.12.0~ce on 16.04
 
 set -o errexit
-set -o verbose
 set -o nounset
 set -o pipefail
+set -o verbose
 
+DIRECTORY="$(dirname ${BASH_SOURCE[0])"
 useradd -G sudo,docker -m -s /bin/bash kacchan
 passwd kacchan
 mkdir -p /home/kacchan/.ssh
 cp /root/.ssh/authorized_keys /home/kacchan/.ssh/authorized_keys
+patch /etc/ssh/sshd_config $DIRECTORY/sshd_config.patch
