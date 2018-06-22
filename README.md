c56db7a6bf9af19328d08bfc821e11c7161824b4
atateno <adrien.tateno@getbraintree.com>
2018-06-22 10:56:33 -0500

    Make bootstrap.sh interactive

--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 ssh -i ~/.ssh/id_rsa root@ip.address
 git clone https://github.com/katsuya94/shizuku.git
 shizuku/bootstrap.sh
+```
+
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 # DigitalOcean Docker 17.12.0~ce on 16.04
+
 set -o errexit
 set -o verbose
 set -o nounset
 set -o pipefail
+set -o interactive
 
-useradd -G sudo,docker kacchan
+useradd -G sudo,docker -m -s /bin/bash kacchan
 mkdir -p /home/kacchan/.ssh
 cp /root/.ssh/authorized_keys /home/kacchan/.ssh/authorized_keys
 su -l kacchan passwd
