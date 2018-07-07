8e8e0bd5a716f338c01b94ed7bb6be70d08e4e19
Adrien Katsuya Tateno <adrien.k.tateno@gmail.com>
2018-07-06 20:52:28 -0500

    Add cron

--- mailstream/main.go
+++ mailstream/main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"context"
 	"log"
+	"time"
 
 	"github.com/katsuya94/shizuku/common"
+	"github.com/robfig/cron"
 )
 
 func main() {
+	c := cron.NewWithLocation(time.UTC)
+	c.AddFunc("0 0 * * * *", deliverNewTransactions)
+	c.Run()
+}
+
+func deliverNewTransactions() {
 	ctx := context.Background()
 	in := NewMailstreamIngester(GetService())
 	err := in.Ingest(ctx, func(transaction *common.Transaction) error {
