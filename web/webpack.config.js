f239d3d75757fc77c8fcb1f0da41c8004e6a38bc
atateno <adrien.tateno@getbraintree.com>
2018-07-20 11:23:18 -0500

    Set up dev server

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -18,6 +18,12 @@ module.exports = {
   },
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'assets')
+  },
+  devServer: {
+    host: '0.0.0.0',
+    publicPath: '/assets/',
+    contentBase: path.resolve(__dirname, 'public'),
+    port: 9000
   }
 };
