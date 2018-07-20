8db5473504f41ed3c0cb86d7f73c3ff318d83b68
atateno <adrien.tateno@getbraintree.com>
2018-07-20 11:59:40 -0500

    Add more node packages

--- web/src/index.js
+++ web/src/index.js
@@ -1,7 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// @flow
 
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('root'),
-);
+import React from "react";
+import ReactDOM from "react-dom";
+import "bulma"
+
+let root = document.getElementById("root");
+if (root) {
+  ReactDOM.render(<h1>Hello, world!</h1>, root);
+}
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,29 +1,33 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['react']
+            presets: ["react"]
           }
         }
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: ["babel-loader", "css-loader", "sass-loader"]
       }
     ]
   },
   output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'assets')
+    filename: "index.js",
+    path: path.resolve(__dirname, "assets")
   },
   devServer: {
-    host: '0.0.0.0',
-    publicPath: '/assets/',
-    contentBase: path.resolve(__dirname, 'public'),
+    host: "0.0.0.0",
+    publicPath: "/assets/",
+    contentBase: path.resolve(__dirname, "public"),
     port: 9000
   }
 };
